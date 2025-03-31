fbec388d4e6032a725f771067f4b9196d90274da
cloudyun233 <1928278661@qq.com>
2025-03-31 13:54:37 +0800

    feat: 初始化项目并添加基础功能
    添加了Spring Boot项目的基本结构，包括主应用类、控制器、实体类、配置文件、测试类以及HTML页面。配置了MyBatis和Spring Security依赖，并设置了基本的Maven构建配置。

---
+++ src/main/java/com/car/jiulongpo/JiulongpoApplication.java
@@ -0,0 +1,13 @@
+package com.car.jiulongpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiulongpoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiulongpoApplication.class, args);
+    }
+
+}
