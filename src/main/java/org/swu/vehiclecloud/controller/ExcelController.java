edcbeb4f28a3f68ff393d53cf09e3855ca6a447e
liuchangyong <1601327696@qq.com>
2025-04-01 20:06:14 +0800

    fix 修复了重构的mybatis依赖和配置文件错误

--- src/main/java/org/swu/vehiclecloud/controller/ExcelController.java
+++ src/main/java/org/swu/vehiclecloud/controller/ExcelController.java
@@ -2,21 +2,28 @@ package org.swu.vehiclecloud.controller;
 
 import org.swu.vehiclecloud.service.ExcelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+/**
+ * Excel导出控制器
+ */
 @RestController
 @RequestMapping("/api/excel")
+@CrossOrigin(origins = "*")
 public class ExcelController {
 
     @Autowired
     private ExcelService excelService;
 
-    @GetMapping("/export")
-    public ResponseEntity<byte[]> exportExcel(@RequestParam String tableName) {
+    /**
+     * 导出Excel文件
+     * @param tableName 要导出的表名
+     * @return 包含Excel文件的响应实体
+     */
+    @PostMapping("/export")
+    public ResponseEntity<Resource> exportExcel(@RequestParam String tableName) {
         return excelService.exportExcel(tableName);
     }
 }
--- src/main/java/org/swu/vehiclecloud/mapper/ExcelMapper.java
+++ src/main/java/org/swu/vehiclecloud/mapper/ExcelMapper.java
@@ -1,10 +1,19 @@
 package org.swu.vehiclecloud.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Excel数据映射接口
+ */
 @Mapper
 public interface ExcelMapper {
+    /**
+     * 查询指定表的所有数据
+     * @param tableName 要查询的表名
+     * @return 包含表数据的Map列表
+     */
     List<Map<String, Object>> selectAllFromTable(String tableName);
 }
