05245d08853d15af5ca4b0f1937acc1fffce36fa
liuchangyong <1601327696@qq.com>
2025-04-03 15:37:19 +0800

    feat mqtt订阅、事件发布

--- src/main/java/org/swu/vehiclecloud/VehicleCloudApplication.java
+++ src/main/java/org/swu/vehiclecloud/VehicleCloudApplication.java
@@ -3,9 +3,12 @@ package org.swu.vehiclecloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.swu.vehiclecloud.config.MqttConfigProperties;
 
 @SpringBootApplication
 @MapperScan("org.swu.vehiclecloud.mapper")
+@EnableConfigurationProperties(MqttConfigProperties.class)
 public class VehicleCloudApplication {
 
     public static void main(String[] args) {
