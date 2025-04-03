d8feffc47a976b7cd601638a27113e852440c9bb
cloudyun233 <1928278661@qq.com>
2025-04-03 14:36:36 +0800

    docs(SSE): 完善SSE相关类的文档注释
    详细描述了SSE工具类、服务接口及实现类的功能、实现细节和注意事项，提高代码可读性和可维护性

--- src/main/java/org/swu/vehiclecloud/service/DataService.java
+++ src/main/java/org/swu/vehiclecloud/service/DataService.java
@@ -3,7 +3,9 @@ package org.swu.vehiclecloud.service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 /**
- * 数据服务接口，定义数据流相关操作
+ * SSE数据流服务接口
+ * <p>定义服务器发送事件(Server-Sent Events)的核心操作，通过实现类提供具体实现</p>
+ * @see DataServiceImpl 默认实现类
  */
 public interface DataService {
     /**
@@ -15,10 +17,11 @@ public interface DataService {
     SseEmitter streamData(String id);
     
     /**
-     * 设置推送内容
-     * @param content 要推送的内容
-     * 设置后，所有已连接的SSE客户端将收到更新后的内容
-     * 内容会每秒推送一次，直到再次更新
+     * 设置指定客户端的推送内容
+     * @param id 通过streamData方法获取的客户端标识符
+     * @param content 需要推送的字符串内容
+     * @throws IllegalArgumentException 当id对应的发射器不存在时抛出
+     * @apiNote 内容设置后会在下次定时任务执行时自动推送，更新内容会覆盖之前的值
      */
     void setPushContent(String id, String content);
 }
