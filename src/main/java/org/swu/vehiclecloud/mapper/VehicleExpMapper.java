18c40d2cbc9dfef098352942fd8c57f42ab3d191
liuyuqi <3051630882@qq.com>
2025-04-10 10:04:28 +0800

    fix 修复了mapper方法名重复导致的报错

--- src/main/java/org/swu/vehiclecloud/mapper/VehicleExpMapper.java
+++ src/main/java/org/swu/vehiclecloud/mapper/VehicleExpMapper.java
@@ -14,7 +14,7 @@ public interface VehicleExpMapper {
     @Insert("INSERT INTO acceleration_exp (vehicleId, accelerationLon, accelerationLat, accelerationVer, timestamp) " +
             "VALUES (#{vehicleId}, #{accelerationLon}, #{accelerationLat}, #{accelerationVer}, #{timestamp})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
-    void insert(AccelerationExp accelerationExp);
+    void insertAccelerationExp(AccelerationExp accelerationExp);
 
     /**
      * 插入速度异常对象
@@ -23,7 +23,7 @@ public interface VehicleExpMapper {
     @Insert("INSERT INTO speed_exp (vehicleId, velocityGNSS, velocityCAN, timestamp) " +
             "VALUES (#{vehicleId}, #{velocityGNSS}, #{velocityCAN}, #{timestamp})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
-    void insert(SpeedExp speedExp);
+    void insertSpeedExp(SpeedExp speedExp);
 
     /**
      * 插入发动机异常对象
@@ -32,7 +32,7 @@ public interface VehicleExpMapper {
     @Insert("INSERT INTO engine_exp (vehicleId, engineSpeed, engineTorque, timestamp) " +
             "VALUES (#{vehicleId}, #{engineSpeed}, #{engineTorque}, #{timestamp})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
-    void insert(EngineExp engineExp);
+    void insertEngineExp(EngineExp engineExp);
 
     /**
      * 插入制动异常对象
@@ -41,7 +41,7 @@ public interface VehicleExpMapper {
     @Insert("INSERT INTO brake_exp (vehicleId, brakeFlag, brakePos, brakePressure, timestamp) " +
             "VALUES (#{vehicleId}, #{brakeFlag}, #{brakePos}, #{brakePressure}, #{timestamp})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
-    void insert(BrakeExp brakeExp);
+    void insertBrakeExp(BrakeExp brakeExp);
 
     /**
      * 插入转向异常对象
@@ -50,7 +50,7 @@ public interface VehicleExpMapper {
     @Insert("INSERT INTO steering_exp (vehicleId, steeringAngle, yawRate, timestamp) " +
             "VALUES (#{vehicleId}, #{steeringAngle}, #{yawRate}, #{timestamp})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
-    void insert(SteeringExp steeringExp);
+    void insertSteeringExp(SteeringExp steeringExp);
 
     /**
      * 插入时间戳异常对象
@@ -59,7 +59,7 @@ public interface VehicleExpMapper {
     @Insert("INSERT INTO timestamp_exp (vehicleId, timestampGNSS, timestamp3, timestamp4, timestamp) " +
             "VALUES (#{vehicleId}, #{timestampGNSS}, #{timestamp3}, #{timestamp4}, #{timestamp})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
-    void insert(TimestampExp timestampExp);
+    void insertTimestampExp(TimestampExp timestampExp);
 
     /**
      * 插入经纬度异常对象
@@ -68,5 +68,5 @@ public interface VehicleExpMapper {
     @Insert("INSERT INTO geo_location_exp (vehicleId, longitude, latitude, timestamp) " +
             "VALUES (#{vehicleId}, #{longitude}, #{latitude}, #{timestamp})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
-    void insert(GeoLocationExp geoLocationExp);
+    void insertGeoLocationExp(GeoLocationExp geoLocationExp);
 }
